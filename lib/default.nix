25b217c50ea134b74d45013eb0e2227d635fb2d2
heather7283 <heather7283@protonmail.com>
2025-11-24 21:17:14 +0400

    fix lib2

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,7 @@
 { lib }:
 
 with builtins; let
-  files = attrNames (removeAttrs (readDir .) [ "default.nix" ]);
-  functions = foldl' (acc: f: acc // (import f { inherit lib; })) {} files;
+  files = attrNames (removeAttrs (readDir ./.) [ "default.nix" ]);
+  functions = foldl' (acc: f: acc // (import ./${f} { inherit lib; })) {} files;
 in functions
 
