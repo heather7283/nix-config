9472c44f82ee0a6f18695ed452c3442c5116ad48
heather7283 <heather7283@protonmail.com>
2026-02-01 22:50:47 +0300

    overlays: update xray to 26.1.23

--- overlays/xray.nix
+++ overlays/xray.nix
@@ -3,16 +3,39 @@
 let
   xray-overlay = final: prev: {
     xray = prev.xray.overrideAttrs (old: rec {
-      version = "25.12.2";
+      version = "26.1.23";
 
       src = prev.fetchFromGitHub {
         owner = "XTLS";
         repo = "Xray-core";
         rev = "v${version}";
-        hash = "sha256-QP6sPeh5j8FJ8sBxYLWB/y66BwAjRk+wJiivGC2xEls=";
+        hash = "sha256-cyrmKC498PuT4thrsGniUFdbKJFseyoQXg4+icaNyT0=";
       };
 
-      vendorHash = "sha256-LzCjzEOREqR108v7zR5jWuDwcrb1K58rpv9RyQUxgic=";
+      patches = let
+        # nixpkgs doesn't have go 1.25.6 yet
+        patch = pkgs.writeTextFile {
+          name = "patch.patch";
+          text = ''
+diff --git a/go.mod b/go.mod
+index 77d8780c..67c402cc 100644
+--- a/go.mod
++++ b/go.mod
+@@ -1,6 +1,6 @@
+ module github.com/xtls/xray-core
+
+-go 1.25.6
++go 1.25.5
+
+ require (
+        github.com/apernet/quic-go v0.57.2-0.20260111184307-eec823306178
+          '';
+        };
+      in [
+        patch
+      ];
+
+      vendorHash = "sha256-9Gk01HouhH3GhAd9vmjWr2UpfxPLTVpJvEbSBBRnRKo=";
     });
   };
 in {
