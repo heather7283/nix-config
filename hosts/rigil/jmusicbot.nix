0b48d344a292636e4cb608a3b2a1ea2bccbd41c6
heather7283 <heather7283@protonmail.com>
2026-02-10 20:01:07 +0000

    rigil: fix jmusicbot config

--- hosts/rigil/jmusicbot.nix
+++ hosts/rigil/jmusicbot.nix
@@ -2,10 +2,21 @@
 
 {
   # see https://jmusicbot.com/config/
-  sops.templates."jmusicbot_config.txt".content = ''
-    token = ${config.sops.placeholder."jmusicbot/token"}
-    owner = ${config.sops.placeholder."jmusicbot/owner"}
-  '';
+  sops.templates."jmusicbot_config.txt" = {
+    content = ''
+      token = ${config.sops.placeholder."jmusicbot/token"}
+      owner = ${config.sops.placeholder."jmusicbot/owner"}
+
+      altprefix = "!"
+
+      aliases {
+        nowplaying = [ np ]
+        play = [ p ]
+        remove = [ delete ]
+      }
+    '';
+    restartUnits = [ "jmusicbot.service" ];
+  };
 
   services.jmusicbot = {
     enable = true;
