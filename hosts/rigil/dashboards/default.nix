755802230cdd65ad2cf4afb35642bb978a1761c4
heather7283 <heather7283@protonmail.com>
2026-03-14 11:10:23 +0000

    rigil: update node exporter grafana dashboard url

--- hosts/rigil/dashboards/default.nix
+++ hosts/rigil/dashboards/default.nix
@@ -4,8 +4,8 @@ let
   remote = {
     "grafana-dashboards/node-exporter-full.json" = {
       source = pkgs.fetchurl {
-        url = "https://github.com/rfmoz/grafana-dashboards/raw/76b2125f29757fc4886b8f25c6fa7ce96878fc4c/prometheus/node-exporter-full.json";
-        hash = "sha256-Y+zsL4I+K3/j2+YoGU/JFp7cYWOhIvil/KkIF/M22fQ=";
+        url = "https://grafana.com/api/dashboards/1860/revisions/42/download";
+        hash = "sha256-pNgn6xgZBEu6LW0lc0cXX2gRkQ8lg/rer34SPE3yEl4=";
       };
     };
   };
