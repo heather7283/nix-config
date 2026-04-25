66a134bfe67f19c3a513f05c3f514b7ff3693b74
heather7283 <heather7283@protonmail.com>
2026-04-25 23:49:03 +0300

    overlays/xray: allow simultaneous usage of forward and reverse proxy

--- overlays/50-xray/default.nix
+++ overlays/50-xray/default.nix
@@ -1,17 +1,21 @@
 { ... }:
 
 final: prev: {
-  unstable.xray = prev.unstable.xray.overrideAttrs (old: rec {
+  unstable.xray = prev.unstable.xray.overrideAttrs (old: let
     version = "26.3.27";
+    patches = with builtins; readDir ./patches |> attrNames |> map (p: ./patches/${p});
+  in {
+    inherit version;
 
-    src = prev.fetchFromGitHub {
+    src = final.fetchFromGitHub {
       owner = "XTLS";
       repo = "Xray-core";
       rev = "v${version}";
       hash = "sha256-tSSoaIKHgLf9ry6p0Y+BM1Nx8X+40BDDfJJYkABUoEc=";
     };
-
     vendorHash = "sha256-kwvck6Eo/e6qgb1ENznhwZ/GPX75ssLUvR2u8Qm3UIM=";
+
+    patches = (old.patches or []) ++ patches;
   });
 }
 
