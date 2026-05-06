8cde7ba0835e2db8c12460821e873b7c0df4c31a
heather7283 <heather7283@protonmail.com>
2026-05-06 20:43:52 +0300

    overlays/xray: do not overwrite unstable package set

--- overlays/50-xray/default.nix
+++ overlays/50-xray/default.nix
@@ -1,21 +1,24 @@
-{ ... }:
+{ lib, ... }:
 
 final: prev: {
-  unstable.xray = prev.unstable.xray.overrideAttrs (old: let
-    version = "26.3.27";
-    patches = with builtins; readDir ./patches |> attrNames |> map (p: ./patches/${p});
-  in {
-    inherit version;
+  # apparently if I don't do this the entire `unstable` attrset get overwritten
+  unstable = prev.unstable // {
+    xray = prev.unstable.xray.overrideAttrs (old: let
+      version = "26.3.27";
+      patches = with builtins; readDir ./patches |> attrNames |> map (p: ./patches/${p});
+    in {
+      inherit version;
 
-    src = final.fetchFromGitHub {
-      owner = "XTLS";
-      repo = "Xray-core";
-      rev = "v${version}";
-      hash = "sha256-tSSoaIKHgLf9ry6p0Y+BM1Nx8X+40BDDfJJYkABUoEc=";
-    };
-    vendorHash = "sha256-kwvck6Eo/e6qgb1ENznhwZ/GPX75ssLUvR2u8Qm3UIM=";
+      src = final.fetchFromGitHub {
+        owner = "XTLS";
+        repo = "Xray-core";
+        rev = "v${version}";
+        hash = "sha256-tSSoaIKHgLf9ry6p0Y+BM1Nx8X+40BDDfJJYkABUoEc=";
+      };
+      vendorHash = "sha256-kwvck6Eo/e6qgb1ENznhwZ/GPX75ssLUvR2u8Qm3UIM=";
 
-    patches = (old.patches or []) ++ patches;
-  });
+      patches = (old.patches or []) ++ patches;
+    });
+  };
 }
 
