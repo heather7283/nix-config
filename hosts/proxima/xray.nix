cb9cbe7fea4feeb6f42acec86523de032d48519b
heather7283 <heather7283@protonmail.com>
2026-05-14 19:06:53 +0300

    proxima: update turn-proxy server binary

--- hosts/proxima/xray.nix
+++ hosts/proxima/xray.nix
@@ -167,9 +167,9 @@ in {
         binary = pkgs.ext.fetchGitHubRelease {
           owner = "Moroka8";
           repo = "vk-turn-proxy";
-          tag = "v1.9.1";
+          tag = "v1.9.2";
           asset = "server-linux-amd64";
-          sha256 = "sha256-V8LTpT6Ea2ojlejY77J3952d2ZHak6hF+PxLy6OwDsI=";
+          sha256 = "sha256-lTaVSG/aQUdquBWa8x1d2KcXTiL79N+aiuZ0GbkbhUg=";
         };
       in pkgs.writeShellScript "vk-turn-proxy-start.sh" ''
         # do not set -x or the key will leak into journal lmao
