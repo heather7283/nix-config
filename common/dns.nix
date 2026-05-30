55787099f72206d3551b6f2deac92e82beae7756
heather7283 <heather7283@protonmail.com>
2026-05-30 12:11:00 +0000

    common/dns: better ipv6 handling

--- common/dns.nix
+++ common/dns.nix
@@ -1,20 +1,30 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  if6 = yes: no: if config.networking.enableIPv6 then yes else no;
+in {
   services.blocky = {
     enable = true;
     settings = let
       nameservers = [
-        [ "https://one.one.one.one/dns-query" "1.1.1.1" "1.0.0.1" ]
-        [ "https://dns.google/dns-query" "8.8.8.8" "8.8.4.4" ]
-        [ "https://dns.quad9.net/dns-query" "9.9.9.9" "149.112.112.112" ]
+        {
+          upstream = "https://one.one.one.one/dns-query";
+          ips = [ "1.1.1.1" "1.0.0.1" ] ++ if6 [ "2606:4700:4700::1111" "2606:4700:4700::1001" ] [];
+        }
+        {
+          upstream = "https://dns.google/dns-query";
+          ips = [ "8.8.8.8" "8.8.4.4" ] ++ if6 [ "2001:4860:4860::8888" "2001:4860:4860::8844" ] [];
+        }
+        {
+          upstream = "https://dns.quad9.net/dns-query";
+          ips = [ "9.9.9.9" "149.112.112.112" ] ++ if6 [ "2620:fe::9" "2620:fe::fe" ] [];
+        }
       ];
-      hasIPv6 = config.networking.enableIPv6;
     in with builtins; {
-      upstreams.groups.default = map (a: head a) nameservers;
-      bootstrapDns = map (a: { upstream = head a; ips = tail a; }) nameservers;
+      upstreams.groups.default = nameservers |> map (ns: ns.upstream);
+      bootstrapDns = nameservers;
 
-      ports.dns = [ "127.0.0.1:53" ] ++ (if hasIPv6 then [ "[::1]:53" ] else []);
+      ports.dns = [ "127.0.0.1:53" ] ++ if6 [ "[::1]:53" ] [];
 
       hostsFile.sources = [ "/etc/hosts" ];
 
@@ -22,15 +32,16 @@
         level = "warn";
         timestamp = false;
       };
-    } // (if hasIPv6 then {} else {
+    } // if6 {} {
       connectIPVersion = "v4";
       filtering.queryTypes = [ "AAAA" ];
-    });
+    };
   };
 
   # see https://discourse.nixos.org/t/why-cant-i-get-dns-nameservers-to-stick/59132
   environment.etc."resolv.conf".text = ''
     nameserver 127.0.0.1
+    ${if6 "nameserver ::1" ""}
   '';
 }
 
