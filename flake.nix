b245579ca7eec5dce616e83dfdff2cc7b5f4d2b0
heather7283 <heather7283@protonmail.com>
2026-05-31 15:07:14 +0000

    flake.lock: update to nixos 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   # apparently functions are not allowed in flake inputs so I have to repeat
   # the `inputs.nixpkgs.follows = "nixpkgs";` mantra for every fucking input
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
