ee1e16225908fc886cf996280fd704273dd37fd4
heather7283 <heather7283@protonmail.com>
2026-05-31 12:35:28 +0000

    common/dns: prevent resolved from being enabled

--- common/dns.nix
+++ common/dns.nix
@@ -42,6 +42,7 @@ in {
     enable = false;
     useLocalResolver = true;
   };
+  services.resolved.enable = lib.mkForce false;
 
   # see https://discourse.nixos.org/t/why-cant-i-get-dns-nameservers-to-stick/59132
   environment.etc."resolv.conf".text = ''
