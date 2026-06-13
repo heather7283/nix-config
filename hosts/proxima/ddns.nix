37281927127c8ddb74db88271fa544da85bba785
heather7283 <heather7283@protonmail.com>
2026-06-13 17:32:21 +0300

    proxima/ddns: remove noip, simplify duckdns

--- hosts/proxima/ddns.nix
+++ hosts/proxima/ddns.nix
@@ -1,7 +1,6 @@
 { pkgs, lib, config, ... }:
 
 {
-  # I couldn't get ddns-updater to work so this will do
   systemd.timers.duckdns-updater = {
     wants = [ "network-online.target" ];
     after = [ "network-online.target" ];
@@ -16,60 +15,18 @@
       name = "duckdns-updater";
       runtimeInputs = [ curl ];
       text = ''
-        ip="$(curl -sS ifconfig.me/ip)"
         domain="$(cat "$CREDENTIALS_DIRECTORY/domain")"
         token="$(cat "$CREDENTIALS_DIRECTORY/token")"
-
-        curl -sS "https://www.duckdns.org/update?domains=$domain&token=$token&ip=$ip"
+        exec curl -fsS "https://www.duckdns.org/update?domains=$domain&token=$token"
       '';
     };
   in {
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${script}/bin/duckdns-updater";
+      ExecStart = lib.getExe script;
       LoadCredential = [
         "domain:${config.sops.secrets."ddns/duckdns/domain".path}"
         "token:${config.sops.secrets."ddns/duckdns/token".path}"
-      ];
-      DynamicUser = true;
-      Restart = "on-failure";
-      RestartSec = "10s";
-    };
-  };
-
-  # Backup in case duckdns is down (has happened before)
-  systemd.timers.noip-updater = {
-    wants = [ "network-online.target" ];
-    after = [ "network-online.target" ];
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnBootSec = "0 sec";
-      OnUnitActiveSec = "5 min";
-    };
-  };
-  systemd.services.noip-updater = let
-    script = with pkgs; writeShellApplication {
-      name = "noip-updater";
-      runtimeInputs = [ curl ];
-      text = ''
-        ip="$(curl -sS ifconfig.me/ip)"
-        hostname="$(cat "$CREDENTIALS_DIRECTORY/hostname")"
-        username="$(cat "$CREDENTIALS_DIRECTORY/username")"
-        password="$(cat "$CREDENTIALS_DIRECTORY/password")"
-
-        curl -sS \
-          -H "Authorization: Basic $(echo "$username:$password" | base64)" \
-          "https://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$ip"
-      '';
-    };
-  in {
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${script}/bin/noip-updater";
-      LoadCredential = [
-        "hostname:${config.sops.secrets."ddns/noip/hostname".path}"
-        "username:${config.sops.secrets."ddns/noip/username".path}"
-        "password:${config.sops.secrets."ddns/noip/password".path}"
       ];
       DynamicUser = true;
       Restart = "on-failure";
