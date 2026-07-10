44b115e564340c96c5089f41b3bf868dc84101fe
heather7283 <heather7283@protonmail.com>
2026-07-10 20:35:40 +0000

    overlays: update xray

--- overlays/50-xray/default.nix
+++ overlays/50-xray/default.nix
@@ -2,7 +2,7 @@
 
 final: prev: {
   xray = prev.unstable.xray.overrideAttrs (old: let
-    version = "26.6.22";
+    version = "26.6.27";
     patches = with builtins; readDir ./patches |> attrNames |> map (p: ./patches/${p});
   in {
     inherit version;
@@ -11,9 +11,9 @@ final: prev: {
       owner = "XTLS";
       repo = "Xray-core";
       rev = "v${version}";
-      hash = "sha256-gSVxRCraw1QdrZfFCjXx6o0MS/3BvXEUO6d3KqZHEhM=";
+      hash = "sha256-NLxG61mCeMwWoNWjDb0JNjMVG5Blp1OnU00RdAkqIdA=";
     };
-    vendorHash = "sha256-4sS3HFLsbusft9UYqmMmIRSJl6LJRnav+d8y6d7B7fY=";
+    vendorHash = "sha256-BSEoAS4jH/Wyosi0xZC7GqgShVmkj2lS5fQQ7p1cT9s=";
 
     patches = (old.patches or []) ++ patches;
   });
