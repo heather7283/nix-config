66d7c5aba495f6f5e3c75d7a256a25fa22dc87eb
heather7283 <heather7283@protonmail.com>
2026-07-18 17:09:44 +0000

    rigil/tor: disable obfs4 tor bridge

--- hosts/rigil/tor.nix
+++ hosts/rigil/tor.nix
@@ -7,33 +7,6 @@ let
   # as the amount of clients that you are hoping to serve concurrently.
   snowflake-max-port = snowflake-min-port + (snowflake-capacity * 2);
 in {
-  services.tor = {
-    enable = true;
-    client.enable = false;
-
-    openFirewall = true;
-    enableGeoIP = true;
-
-    relay = {
-      enable = true;
-      role = "bridge";
-    };
-    settings = {
-      ORPort = 27383;
-      ExitRelay = false;
-      BridgeRelay = true;
-      Nickname = "skebob";
-
-      BandwidthRate = "50 MBits";
-      BandwidthBurst = "100 MBits";
-
-      BridgeRecordUsageByCountry = true;
-
-      MetricsPort = "127.0.0.1:27384 prometheus";
-      MetricsPortPolicy = "accept 127.0.0.1";
-    };
-  };
-
   services.snowflake-proxy = {
     enable = true;
     capacity = snowflake-capacity;
