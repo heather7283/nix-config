abcab4945c39192b23c4404de11b62703319f272
heather7283 <heather7283@protonmail.com>
2026-07-25 16:46:17 +0000

    rigil: set up omniroute

--- hosts/rigil/omniroute.nix
+++ hosts/rigil/omniroute.nix
@@ -1,15 +1,15 @@
 { config, pkgs, lib, ... }:
 
-{
-  users = let
-    id = 992; # prayge that it's not alredy taken (cuxos doesn't guard against this)
-  in {
+let
+  uid = 992; # prayge that it's not alredy taken (cuxos doesn't guard against this)
+in {
+  users = {
     users.omniroute = {
-      uid = id;
+      inherit uid;
       group = "omniroute";
     };
     groups.omniroute = {
-      gid = id;
+      gid = uid;
     };
   };
 
@@ -25,11 +25,20 @@
     image = "diegosouzapw/omniroute:latest";
     volumes = [ "/var/lib/omniroute:/app/data" ];
     ports = [ "127.0.0.1:20128:20128" ];
-    #environment = {
-    #  ENABLE_SOCKS5_PROXY = "true";
-    #  ALL_PROXY = "TODO";
-    #  ENABLE_TLS_FINGERPRINT = "true";
-    #};
+    extraOptions = [ "--network=host" ]; # idk how else I can make it see the proxy
+
+    environment = {
+      ENABLE_SOCKS5_PROXY = "true";
+      NEXT_PUBLIC_ENABLE_SOCKS5_PROXY = "true";
+      ALL_PROXY = "socks5://127.0.0.1:10808"; # xray socks inbound that will send it through warp
+      PROXY_FAIL_OPEN = "false";
+      #ENABLE_TLS_FINGERPRINT = "true"; # Couldn't get it to work with proxy for whatever reason
+    };
+  };
+  systemd.services.omniroute = rec {
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" "dante-omniroute.service" ];
+    after = wants;
   };
 }
 
