e701bc2593e25dcaef8922059eed26615805c5df
heather7283 <heather7283@protonmail.com>
2026-08-27 15:57:43 +0000

    rigil/omniroute: switch to quadlet

--- hosts/rigil/omniroute.nix
+++ hosts/rigil/omniroute.nix
@@ -19,26 +19,30 @@ in {
     mode = "0700";
   };
 
-  virtualisation.oci-containers.containers.omniroute = {
-    serviceName = "omniroute";
-    user = with config.users; "${toString users.omniroute.uid}:${toString groups.omniroute.gid}";
-    image = "diegosouzapw/omniroute:latest";
-    volumes = [ "/var/lib/omniroute:/app/data" ];
-    ports = [ "127.0.0.1:20128:20128" ];
-    extraOptions = [ "--network=host" ]; # idk how else I can make it see the proxy
+  virtualisation.quadlet.containers.omniroute = {
+    containerConfig = {
+      Image = "docker.io/diegosouzapw/omniroute:latest";
+      AutoUpdate = "registry";
 
-    environment = {
-      ENABLE_SOCKS5_PROXY = "true";
-      NEXT_PUBLIC_ENABLE_SOCKS5_PROXY = "true";
-      ALL_PROXY = "socks5://127.0.0.1:10808"; # xray socks inbound that will send it through warp
-      PROXY_FAIL_OPEN = "false";
-      #ENABLE_TLS_FINGERPRINT = "true"; # Couldn't get it to work with proxy for whatever reason
+      User = toString config.users.users.omniroute.uid;
+      Group = toString config.users.groups.omniroute.gid;
+      Volume = [ "/var/lib/omniroute:/app/data" ];
+
+      #PublishPort = [ "127.0.0.1:20128:20128" ];
+      Network = "host"; # idk how else I can make it see the proxy
+
+      Environment = {
+        ENABLE_SOCKS5_PROXY = "true";
+        NEXT_PUBLIC_ENABLE_SOCKS5_PROXY = "true";
+        ALL_PROXY = "socks5://127.0.0.1:10808"; # xray socks inbound that will send it through warp
+        PROXY_FAIL_OPEN = "false";
+        ENABLE_TLS_FINGERPRINT = "true"; # Couldn't get it to work with proxy for whatever reason
+      };
     };
-  };
-  systemd.services.omniroute = rec {
-    wantedBy = [ "multi-user.target" ];
-    wants = [ "network-online.target" "dante-omniroute.service" ];
-    after = wants;
+    unitConfig = rec {
+      Wants = [ "network-online.target" ];
+      After = Wants;
+    };
   };
 }
 
