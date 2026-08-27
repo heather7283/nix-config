3fc593c64c0e0ff13667b7d247d732c6c41e4e46
heather7283 <heather7283@protonmail.com>
2026-08-27 15:27:07 +0000

    rigil/jmusicbot: move to quadlet

--- hosts/rigil/jmusicbot.nix
+++ hosts/rigil/jmusicbot.nix
@@ -22,32 +22,36 @@
     restartUnits = [ "jmusicbot.service" ];
   };
 
-  virtualisation.oci-containers.containers.jmusicbot = {
-    serviceName = "jmusicbot";
-    image = "eclipse-temurin:25-jre";
-    volumes = let
-      jar = pkgs.ext.fetchGitHubRelease rec {
-        owner = "Cosgy-Dev";
-        repo = "JMusicBot-JP";
-        tag = "0.11.0";
-        asset = "JMusicBot-${tag}-All.jar";
-        sha256 = "sha256-cVlugGrTLGO7YdaT8Ywx2kaNqcKEPUMR/yjF6HetyMk=";
-      };
-      cfg = config.sops.templates."jmusicbot_config.txt".path;
-    in [
-      "${jar}:/jmusicbot.jar:ro"
-      "${cfg}:/config.txt:ro"
-    ];
-    workdir = "/";
-    entrypoint = "java";
-    cmd = [
-      "--enable-native-access=ALL-UNNAMED"
-      "-Dnogui=true"
-      "-Dconfig.file=/config.txt"
-      "-jar" "/jmusicbot.jar"
-    ];
+  virtualisation.quadlet.containers.jmusicbot = {
+    containerConfig = {
+      Image = "docker.io/library/eclipse-temurin:25-jre";
+      Volume = let
+        jar = pkgs.ext.fetchGitHubRelease rec {
+          owner = "Cosgy-Dev";
+          repo = "JMusicBot-JP";
+          tag = "0.11.0";
+          asset = "JMusicBot-${tag}-All.jar";
+          sha256 = "sha256-cVlugGrTLGO7YdaT8Ywx2kaNqcKEPUMR/yjF6HetyMk=";
+        };
+        cfg = config.sops.templates."jmusicbot_config.txt".path;
+      in [
+        "${jar}:/jmusicbot.jar:ro"
+        "${cfg}:/config.txt:ro"
+      ];
+      WorkingDir = "/";
+      Entrypoint = "java";
+      Exec = builtins.concatStringsSep " " [
+        "--enable-native-access=ALL-UNNAMED"
+        "-Dnogui=true"
+        "-Dconfig.file=/config.txt"
+        "-jar" "/jmusicbot.jar"
+      ];
+      NoNewPrivileges = true;
+    };
+    unitConfig = rec {
+      Wants = [ "network-online.target" ];
+      After = Wants;
+    };
   };
-
-  systemd.services.jmusicbot.enable = true;
 }
 
