f75f6043dc5cc4d47ee7c22282d2409346c05256
Heather <heather7283@protonmail.com>
2026-09-11 13:14:53 +0400

    common: add my own cachix

--- common/default.nix
+++ common/default.nix
@@ -9,10 +9,12 @@
     extra-substituters = [
       "https://cache.numtide.com"
       "https://nix-community.cachix.org"
+      "https://heather7283.cachix.org"
     ];
     extra-trusted-public-keys = [
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "heather7283.cachix.org-1:hyGLOvsVUl6bkGvUKqsOxH06ZPhaEy8Z6HIiEPsQ39c="
     ];
   };
 
