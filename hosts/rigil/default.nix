2d2fd4ec410ab0d5566182c8b39767dcb2a01879
heather7283 <heather7283@protonmail.com>
2026-09-12 11:58:16 +0000

    rigil: enable systemd-network-wait-online.service
    also move networking stuff into a separate file

--- hosts/rigil/default.nix
+++ hosts/rigil/default.nix
@@ -23,49 +23,6 @@
     configFile = config.sops.secrets."ycurator/config.json".path;
   };
 
-  networking = {
-    useDHCP = false;
-    useNetworkd = true;
-
-    nftables.enable = true;
-
-    firewall = {
-      enable = true;
-      trustedInterfaces = [ "wg0" ];
-      allowedTCPPorts = [
-        5201 # iperf3
-      ];
-      allowedUDPPorts = [
-      ];
-    };
-  };
-
-  systemd.network.enable = true;
-  systemd.network.wait-online.enable = false;
-  # things I have to do to not flash my VPS' public IP on github :|
-  sops.templates."ens3.network" = {
-    content = let
-      ph = config.sops.placeholder;
-    in ''
-      [Match]
-      Name=ens3
-
-      [Network]
-      Address=${ph."ip/v4/address"}
-      Address=${ph."ip/v6/address"}
-
-      [Route]
-      Gateway=${ph."ip/v4/gateway"}
-      GatewayOnLink=true
-
-      [Route]
-      Gateway=${ph."ip/v6/gateway"}
-    '';
-    path = "/etc/systemd/network/ens3.network";
-    mode = "0644";
-    restartUnits = [ "systemd-networkd.service" ];
-  };
-
   virtualisation.quadlet = {
     enable = true;
     autoUpdate = {
