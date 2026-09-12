00a6836de4570dbe46bb92b0d8593404e05c7fc9
Heather <heather7283@protonmail.com>
2026-09-13 01:41:04 +0400

    overlays/zsh: update sqlite-history patch

--- overlays/50-zsh/default.nix
+++ overlays/50-zsh/default.nix
@@ -1,12 +1,18 @@
-{ ... }:
+{ lib, ... }:
 
 final: prev: {
   zsh = prev.zsh.overrideAttrs (old: let
     patches = with builtins; readDir ./patches |> attrNames |> map (p: ./patches/${p});
+    sqlite = final.pkgs.sqlite;
   in {
     patches = (old.patches or []) ++ patches;
-    configureFlags = (old.configureFlags or []) ++ [ "--enable-sqlite" ];
-    buildInputs = (old.buildInputs or []) ++ (with final.pkgs; [ sqlite pkg-config ]);
+    buildInputs = (old.buildInputs or []) ++ [ sqlite ]; # is this needed at all?
+    postFixup = (old.postFixup or "") + ''
+      # thanks box
+      patchelf --add-rpath \
+        ${lib.getLib sqlite}/lib \
+        $out/lib/zsh/${old.version}/x_heather7283/sqlite_history.so
+    '';
   });
 }
 
